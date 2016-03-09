eb190981e26c6890b35923e33c792eab7b0f7ab4
1000io <1000io.com@gmail.com>
2016-03-09 16:27:50 +0100

    Update test.cpp
    New log file option

--- test.cpp
+++ test.cpp
@@ -15,6 +15,19 @@ int main(int argc, char *argv[])
 {
 	int RXPIN = 1;
 	int TXPIN = 0;
+        int loggingok;   // Global var indicating logging on or off
+        FILE *fp;        // Global var file handle
+        
+        if(argc==2) {
+          fp = fopen(argv[1], "a"); // Log file opened in append mode to avoid destroying data
+          loggingok=1;
+          if (fp == NULL) {
+              perror("Failed to open log file!"); // Exit if file open fails
+              exit(EXIT_FAILURE);
+          }
+        } else {
+          loggingok=0;
+        }
 
 	if(wiringPiSetup() == -1)
 		return 0;
@@ -36,6 +49,11 @@ int main(int argc, char *argv[])
 				printf("Temp : %f\n",s->getTemperature());
 				printf("Humidity : %f\n",s->getHumidity());
 				printf("Channel : %d\n",s->getChannel());
+                                if((loggingok) && (s->getChannel()>0)) {
+                                        fprintf(fp,"%d,temp%f,hum%f\n",s->getChannel(),s->getTemperature(),s->getHumidity());
+                                        fflush(fp);
+                                        fflush(stdout);
+                                }
 			}
 			delete s;
 		}
